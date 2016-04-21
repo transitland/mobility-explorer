3f26e2ab87aec6b0968eb19e17af67a7c63f334a
Meghan <mmhade@gmail.com>
2016-04-21 16:31:24 -0700

    add leaflet

---
+++ app/index/controller.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  lat: 45.519743,
+  lng: -122.680522,
+  zoom: 12,
+  emberConfLocation: [45.528298, -122.662986],
+  hotel: [45.530891, -122.655504],
+  actions: {
+    updateCenter(e) {
+      let center = e.target.getCenter();
+      this.set('lat', center.lat);
+      this.set('lng', center.lng);
+    }
+  }
+});
