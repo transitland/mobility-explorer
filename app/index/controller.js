f13c82b26a705efaa3cd0781c942a0c6260ed1a8
Meghan <mmhade@gmail.com>
2016-06-14 17:52:04 -0700

    add side nav, get bbox bounds, z-index

--- app/index/controller.js
+++ app/index/controller.js
@@ -4,6 +4,7 @@ export default Ember.Controller.extend({
   lat: 45.519743,
   lng: -122.680522,
   zoom: 12,
+  bBox: null,
   emberConfLocation: [45.528298, -122.662986],
   hotel: [45.530891, -122.655504],
   icon: L.icon({
@@ -15,6 +16,11 @@ export default Ember.Controller.extend({
       let center = e.target.getCenter();
       this.set('lat', center.lat);
       this.set('lng', center.lng);
+    },
+    getBbox(e) {
+      let bBox = e.target.getBounds();
+      this.set('bBox', bBox.toBBoxString());
+
     }
   }
 });
