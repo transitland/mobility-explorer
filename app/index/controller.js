8f84331a5c971e787365af60605fc75ee50b78fd
Meghan <mmhade@gmail.com>
2016-06-15 11:27:38 -0700

    add nested radio options

--- app/index/controller.js
+++ app/index/controller.js
@@ -5,12 +5,27 @@ export default Ember.Controller.extend({
   lng: -122.680522,
   zoom: 12,
   bBox: null,
+  selectedType: null,
+  resetButton: false,
   emberConfLocation: [45.528298, -122.662986],
   hotel: [45.530891, -122.655504],
   icon: L.icon({
     iconUrl: 'assets/images/marker.png',
     iconSize: (20, 20)
   }),
+
+  routesSelected: Ember.computed('selectedType', function() {
+    return (this.get('selectedType') === "routes");
+  }),
+
+  stopsSelected: Ember.computed('selectedType', function() {
+    return (this.get('selectedType') === "stops");
+  }),
+
+  operatorsSelected: Ember.computed('selectedType', function() {
+    return (this.get('selectedType') === "operators");
+  }),
+
   actions: {
     updateCenter(e) {
       let center = e.target.getCenter();
@@ -20,7 +35,16 @@ export default Ember.Controller.extend({
     getBbox(e) {
       let bBox = e.target.getBounds();
       this.set('bBox', bBox.toBBoxString());
-
+    },
+    toggleSelectedType(type){
+      this.set('selectedType', type);
+    },
+    toggleReset(){
+      this.toggleProperty('resetButton');
     }
   }
 });
+
+// supportedImportLevel: Ember.computed('importLevel', function() {
+//     return (this.get('importLevel') >= 0 && this.get('importLevel') <= 4);
+//   })
