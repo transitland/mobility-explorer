d6318a606c224130d2d3d7c7d29644d07d9aeac9
Meghan <mmhade@gmail.com>
2016-06-17 12:50:51 -0700

    move operator menu to operators route

--- app/index/controller.js
+++ app/index/controller.js
@@ -1,25 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	selectedType: "initial",
 	bBox: null,
-	
+
   
-  initialSelected: Ember.computed('selectedType', function() {
-		return (this.get('selectedType') === "initial");
-	}),
 
-	routesSelected: Ember.computed('selectedType', function() {
-		return (this.get('selectedType') === "routes");
-	}),
-
-	stopsSelected: Ember.computed('selectedType', function() {
-		return (this.get('selectedType') === "stops");
-	}),
-
-	operatorsSelected: Ember.computed('selectedType', function() {
-		return (this.get('selectedType') === "operators");
-	}),
 
 	actions: {
 		getBbox(e) {
