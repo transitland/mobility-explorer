796405da6c54f2d0c05dc89e0734d80d3a743709
Meghan <mmhade@gmail.com>
2016-06-20 16:20:12 -0700

    add details for stop and route models

--- app/data/transitland/route/model.js
+++ app/data/transitland/route/model.js
@@ -1,5 +1,19 @@
-import Model from 'ember-data/model';
+import Ember from 'ember';
+import DS from 'ember-data';
 
-export default Model.extend({
-  
+var Route = DS.Model.extend({
+	identifiers: DS.attr(),
+	name: DS.attr('string'),
+	short_name: DS.attr('string'),
+	onestop_id: Ember.computed.alias('id'),
+	geometry: DS.attr(),
+	tags: DS.attr(),
+	country: DS.attr('string'),
+	state: DS.attr('string'),
+	metro: DS.attr('string'),
+	timezone: DS.attr('string'),
+	created_at: DS.attr('date'),
+	updated_at: DS.attr('date'),
 });
+
+export default Route;
--- app/data/transitland/stop/model.js
+++ app/data/transitland/stop/model.js
@@ -1,5 +1,17 @@
-import Model from 'ember-data/model';
+import Ember from 'ember';
+import DS from 'ember-data';
 
-export default Model.extend({
-  
+var Stop = DS.Model.extend({
+	identifiers: DS.attr(),
+	onestop_id: Ember.computed.alias('id'),
+	geometry: DS.attr(),
+	name: DS.attr('string'),
+	tags: DS.attr(),
+	timezone: DS.attr('string'),
+	created_at: DS.attr('date'),
+	updated_at: DS.attr('date'),
+	operators_serving_stop: DS.attr(),
+	routes_serving_stop: DS.attr(),
 });
+
+export default Stop;
