eaa8bab252ec7b8be570588d501fd0998e6dcc21
Meghan <mmhade@gmail.com>
2016-06-22 11:30:29 -0700

    render routes as plyline layers

--- app/routes/controller.js
+++ app/routes/controller.js
@@ -4,12 +4,18 @@ import mapBboxController from 'mobility-playground/mixins/map-bbox-controller';
 export default Ember.Controller.extend(mapBboxController, {
 	queryParams: ['bBox'],
 	bBox: null,
-
+	lat: 37.7749,
+	lng: -122.4194,
+	zoom: 12,
+	icon: L.icon({
+		iconUrl: 'assets/images/marker.png',		
+		iconSize: (20, 20)
+	}),
 	actions: {
-    setBbox(newBbox) {
-	  	let bBox = newBbox;
-	    this.set('bBox', bBox.toBBoxString());
-    },
+    updateBbox(e) {
+			var newBox = e.target.getBounds();
+			this.set('bBox', newBox.toBBoxString());
+		},
     printBbox(){
       console.log("routes bbox: " + this.bBox);
     }
--- app/stops/controller.js
+++ app/stops/controller.js
@@ -18,4 +18,3 @@ export default Ember.Controller.extend(mapBboxController, {
 		}
 	}	
 });
-
