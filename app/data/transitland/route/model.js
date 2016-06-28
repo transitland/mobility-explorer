2dd583e47bb069f2bafcc9d173e2079a584c1e0d
Meghan <mmhade@gmail.com>
2016-06-27 18:10:51 -0700

    add query param to view routes serving stop, start to add logic to color route polylines by operator onestop id

--- app/data/transitland/route/model.js
+++ app/data/transitland/route/model.js
@@ -34,7 +34,19 @@ var Route = DS.Model.extend({
 			reversedCoordArray.push(coordArray);
 		}
 		return reversedCoordArray;
-	}).property('geometry')
+	}).property('geometry'),
+
+	operator_color: (function(){
+		// if (this.get('operated_by_onestop_id') === 'o-9q8y-sfmta'){
+		// 	return 'red';
+		// } else {
+		// 	return 'yellow';
+		// }
+		
+		// write funtion that creates a hash
+
+
+	}).property('operated_by_onestop_id')
 });
 
 export default Route;
