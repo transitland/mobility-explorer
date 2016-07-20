1f1258703544fbfac56173a0b6193e464da7a991
Meghan <mmhade@gmail.com>
2016-07-20 16:06:21 -0700

    new pattern for passing bbox to map, index only

--- app/index/controller.js
+++ app/index/controller.js
@@ -5,40 +5,46 @@ import mapBboxController from 'mobility-playground/mixins/map-bbox-controller';
 export default Ember.Controller.extend(mapBboxController, {
 	queryParams: ['bbox'],
 	bbox: null,
+	displayBbox: [[37.706911598228466, -122.54287719726562],[37.84259697150785, -122.29568481445312]],
 	place: null,
-	zoom: 12,
-	lat: 37.778008,
-	lng: -122.431272,
-	bounds: Ember.computed('bbox', function(){
-		if (this.get('bbox') === null){
-			// -122.54287719726562%2C37.706911598228466%2C-122.29568481445312%2C37.84259697150785
-			var defaultBoundsArray = [];
-			defaultBoundsArray.push([37.706911598228466, -122.54287719726562]);
-			defaultBoundsArray.push([37.84259697150785, -122.29568481445312]);
-			return defaultBoundsArray;
-		} else {
-			var coordinateArray = [];
-			var bboxString = this.get('bbox');
-			var tempArray = [];
-			var boundsArray = [];
+	// bounds: Ember.computed('bbox', function(){
+	// 	console.log("bounds fired");
+	// 	if (this.get('displayBbox') === null) {
+	// 		if (this.get('bbox') === null){
+	// 			console.log("bounds null");
 
-			coordinateArray = bboxString.split(',');
+	// 			var defaultBoundsArray = [[37.706911598228466, -122.54287719726562],[37.84259697150785, -122.29568481445312]];
+	// 			this.set('displayBbox', defaultBoundsArray);
+	// 			console.log(this.get('displayBbox'));			
 
-			for (var i = 0; i < coordinateArray.length; i++){
-				tempArray.push(parseFloat(coordinateArray[i]));
-			}
-		
-			var arrayOne = [];
-			var arrayTwo = [];
-			arrayOne.push(tempArray[1]);
-			arrayOne.push(tempArray[0]);
-			arrayTwo.push(tempArray[3]);
-			arrayTwo.push(tempArray[2]);
-			boundsArray.push(arrayOne);
-			boundsArray.push(arrayTwo);
-			return boundsArray;
-		}
-	}),
+	// 			return defaultBoundsArray;
+	// 		} else {
+	// 			console.log("bounds not null");
+	// 			var coordinateArray = [];
+	// 			var bboxString = this.get('bbox');
+	// 			var tempArray = [];
+	// 			var boundsArray = [];
+
+	// 			coordinateArray = bboxString.split(',');
+
+	// 			for (var i = 0; i < coordinateArray.length; i++){
+	// 				tempArray.push(parseFloat(coordinateArray[i]));
+	// 			}
+			
+	// 			var arrayOne = [];
+	// 			var arrayTwo = [];
+	// 			arrayOne.push(tempArray[1]);
+	// 			arrayOne.push(tempArray[0]);
+	// 			arrayTwo.push(tempArray[3]);
+	// 			arrayTwo.push(tempArray[2]);
+	// 			boundsArray.push(arrayOne);
+	// 			boundsArray.push(arrayTwo);
+	// 			console.log(this.get('displayBbox'));
+	// 			this.set('displayBbox', boundsArray);
+	// 			return boundsArray;
+	// 		}
+	// 	}
+	// }),
 	icon: L.icon({
 		iconUrl: 'assets/images/marker.png',		
 		iconSize: (20, 20)
@@ -71,7 +77,7 @@ export default Ember.Controller.extend(mapBboxController, {
 
   	setPlace: function(selected){
   		this.set('place', selected);
-  		this.set('bbox', selected.coordinates);
+  		this.set('bbox', selected.bbox);
   	},
   	clearPlace: function(){
   		this.set('place', null);
