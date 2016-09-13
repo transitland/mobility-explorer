d8a3310ba9c1f64d1fa5f8c6561ecb334cabc746
Meghan <mmhade@gmail.com>
2016-09-13 12:00:24 -0700

    revise stops and routes to render component

--- app/components/url-builder/component.js
+++ app/components/url-builder/component.js
@@ -1,10 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	queryUrl: Ember.computed('bbox', 'onestop_id', function(){
-		// var url = "https://transit.land/api/v1/operators?";
+	queryUrl: Ember.computed('bbox', 'onestop_id', 'serves', 'operated_by', 'vehicle_type', 'served_by', function(){
 		var url = "https://transit.land/api/v1/" + this.entity + "?"
-		var arrayOfQueryParams = [];
+		var arrayOfQueryParams = ['style_routes_by', 'isochrone_mode'];
 		console.log(this.type);
 		for (var i = 0; i < this.get('queryParams').length; i++){
 			if (arrayOfQueryParams.indexOf(this.get('queryParams')[i]) === -1 && this.get(this.get('queryParams')[i]) !== null){
