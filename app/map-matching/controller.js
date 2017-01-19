48eb255de7e2540fae2ad4079bd99eb5873cbdb6
Meghan <mmhade@gmail.com>
2017-01-19 15:30:12 -0800

    add map-matching route and controller

---
+++ app/map-matching/controller.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+import mapBboxController from 'mobility-playground/mixins/map-bbox-controller';
+import setTextboxClosed from 'mobility-playground/mixins/set-textbox-closed';
+import sharedActions from 'mobility-playground/mixins/shared-actions';
+
+export default Ember.Controller.extend(mapBboxController, setTextboxClosed, sharedActions, {
+  queryParams: ['bbox','pin'],
+  center: Ember.computed('pin', function(){
+    if (this.get('pin')){
+      return this.get('pinLocation');
+    } else {
+      return this.get('mapCenter');
+    }
+  }),
+  zoom: 14,
+
+  actions: {
+    updatebbox(e) {
+      var newbox = e.target.getBounds();
+      this.set('bbox', newbox.toBBoxString());
+    },
+    setIsochroneMode: function(mode){
+      if (this.get('isochrone_mode') === mode){
+        this.set('isochrone_mode', null);
+      } else {
+        this.set('isochrone_mode', mode);
+      }
+    }
+  }
+});
---
+++ app/map-matching/route.js
@@ -0,0 +1,46 @@
+import Ember from 'ember';
+import setLoading from 'mobility-playground/mixins/set-loading';
+
+export default Ember.Route.extend(setLoading, {
+	queryParams: {
+    bbox: {
+      replace: true,
+      refreshModel: true
+    },
+    pin: {
+    	replace: true,
+    }
+	},
+	setupController: function (controller, model) {
+		if (controller.get('bbox') !== null){
+			var coordinateArray = [];
+			var bboxString = controller.get('bbox');
+			var tempArray = [];
+			var boundsArray = [];
+
+			coordinateArray = bboxString.split(',');
+
+			for (var i = 0; i < coordinateArray.length; i++){
+				tempArray.push(parseFloat(coordinateArray[i]));
+			}
+
+			var arrayOne = [];
+			var arrayTwo = [];
+			arrayOne.push(tempArray[1]);
+			arrayOne.push(tempArray[0]);
+			arrayTwo.push(tempArray[3]);
+			arrayTwo.push(tempArray[2]);
+			boundsArray.push(arrayOne);
+			boundsArray.push(arrayTwo);
+			controller.set('leafletBounds', boundsArray);
+		}
+    controller.set('leafletBbox', controller.get('bbox'));
+    this._super(controller, model);
+
+	},
+	actions: {
+	}
+});
+
+
+
