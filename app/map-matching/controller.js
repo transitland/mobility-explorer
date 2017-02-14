0b9831ad0459eba02961f954ba60cf7442dbfe8f
Meghan <mmhade@gmail.com>
2017-02-14 14:41:14 -0800

    import xml parser into map-mathing route and controller

--- app/map-matching/controller.js
+++ app/map-matching/controller.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import mapBboxController from 'mobility-playground/mixins/map-bbox-controller';
 import setTextboxClosed from 'mobility-playground/mixins/set-textbox-closed';
 import sharedActions from 'mobility-playground/mixins/shared-actions';
+import xml2js from 'npm:xml2js';
+
 
 export default Ember.Controller.extend(mapBboxController, setTextboxClosed, sharedActions, {
   queryParams: ['bbox','pin'],
@@ -13,18 +15,11 @@ export default Ember.Controller.extend(mapBboxController, setTextboxClosed, shar
     }
   }),
   zoom: 14,
-
+ 
   actions: {
     updatebbox(e) {
       var newbox = e.target.getBounds();
       this.set('bbox', newbox.toBBoxString());
-    },
-    setIsochroneMode: function(mode){
-      if (this.get('isochrone_mode') === mode){
-        this.set('isochrone_mode', null);
-      } else {
-        this.set('isochrone_mode', mode);
-      }
     }
   }
 });
--- app/map-matching/route.js
+++ app/map-matching/route.js
@@ -1,15 +1,9 @@
 import Ember from 'ember';
 import setLoading from 'mobility-playground/mixins/set-loading';
+import xml2js from 'npm:xml2js';
 
 export default Ember.Route.extend(setLoading, {
 	queryParams: {
-    bbox: {
-      replace: true,
-      refreshModel: true
-    },
-    pin: {
-    	replace: true,
-    }
 	},
 	setupController: function (controller, model) {
 		if (controller.get('bbox') !== null){
@@ -33,9 +27,29 @@ export default Ember.Route.extend(setLoading, {
 			boundsArray.push(arrayOne);
 			boundsArray.push(arrayTwo);
 			controller.set('leafletBounds', boundsArray);
+
 		}
-    controller.set('leafletBbox', controller.get('bbox'));
-    this._super(controller, model);
+		controller.set('leafletBbox', controller.get('bbox'));
+		this._super(controller, model);
+
+	},
+	model: function(params){
+		this.store.unloadAll('data/transitland/operator');
+		this.store.unloadAll('data/transitland/stop');
+		this.store.unloadAll('data/transitland/route');
+		this.store.unloadAll('data/transitland/route_stop_pattern');
+
+		var trace;
+			
+		var gpx = '<?xml version="1.0" encoding="UTF-8"?><gpx creator="strava.com iPhone" version="1.1" xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd"><metadata><time>2017-02-12T18:16:26Z</time></metadata><trk><name>Morning Run</name><trkseg><trkpt lat="37.7499690" lon="-122.5083000"><ele>8.1</ele><time>2017-02-12T18:16:26Z</time></trkpt><trkpt lat="37.7499620" lon="-122.5083530"><ele>8.2</ele><time>2017-02-12T18:16:28Z</time></trkpt><trkpt lat="37.7499610" lon="-122.5083540"><ele>8.2</ele><time>2017-02-12T18:16:30Z</time></trkpt></trkseg></trk></gpx>';
+  	xml2js.parseString(gpx, function (err, result){
+	     trace = result.gpx.trk;
+	   });
+  	console.log(trace);
+
+		return Ember.RSVP.hash({
+			trace: trace
+		});
 
 	},
 	actions: {
