06dd9ec1e32ae85f123f2085f9b8ae237a92aac0
Meghan <mmhade@gmail.com>
2017-03-28 14:00:18 -0700

    removing XMLSerializer resolved issue

--- app/map-matching/route.js
+++ app/map-matching/route.js
@@ -104,13 +104,6 @@ export default Ember.Route.extend(setLoading, {
 			contentType: "text/xml",
 			url: 'assets/traces/' + gpxTrace.filename,
 		})
-		// test for dev:
-		// .then(function(response) {
-		// 	// look into xpath to query xml dom
-		// 	var s = new XMLSerializer();
-		// 	var str = s.serializeToString(response);
-		// 	return str;
-		// })
 	},
 
 	getGPXTrace: function(gpxTrace) {
