3af3ae618213e171d19faafb2ca787b444bcec96
Meghan <mmhade@gmail.com>
2017-06-16 16:48:19 -0700

    add zoom to segment option, separate link to OSM

--- app/map-matching/controller.js
+++ app/map-matching/controller.js
@@ -12,6 +12,14 @@ export default Ember.Controller.extend(mapBboxController, setTextboxClosed, shar
   zoom: 14,
   trace: null,
   costing: null,
+  traceBounds: Ember.computed('selectedDiscontinuity', function(){
+    if (this.selectedDiscontinuity){
+      return this.selectedDiscontinuity.edgeCoordinates;
+    }
+    else {
+      return this.model.gpxTrace.bounds;
+    }
+  }),
   uploading: false,
   showTraceAttribut: false,
   showTraceRoute: false,
@@ -327,7 +335,8 @@ export default Ember.Controller.extend(mapBboxController, setTextboxClosed, shar
       var newbox = e.target.getBounds();
       this.set('bbox', newbox.toBBoxString());
     },
-    setTrace(trace){      
+    setTrace(trace){    
+      this.set('selectedDiscontinuity', null);  
       this.set('trace', null);
       this.set('costing', null);
       this.set('showErrorMessage', false);
@@ -436,5 +445,5 @@ export default Ember.Controller.extend(mapBboxController, setTextboxClosed, shar
     unselectDiscontinuity(){
       this.set('selectedDiscontinuity', null);
     }
-  },
+  }
 });
